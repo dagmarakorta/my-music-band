aa895868878ef2dda13db793fcdaf3502dde5211
Dagmara Korta <dagmara.korta@gmail.com>
2022-08-21 10:07:46 +0200

    created a join table for users and instruments

--- db/migrate/20220821080633_create_table_user_instruments.rb
+++ db/migrate/20220821080633_create_table_user_instruments.rb
@@ -3,6 +3,7 @@ class CreateTableUserInstruments < ActiveRecord::Migration[7.0]
     create_table :table_user_instruments do |t|
       t.references :user, foreign_key: true
       t.references :instrument, foreign_key: true
+
       t.timestamps
     end
   end
