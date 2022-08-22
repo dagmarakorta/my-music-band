f1a866adb611c81cd3151da695789f97b2a20396
Dagmara Korta <dagmara.korta@gmail.com>
2022-08-22 18:19:48 +0200

    corrected permited parameters for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :latitude, :longitude])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :lat, :long])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :latitude, :longitude])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :lat, :long])
   end
 end
