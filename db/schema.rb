259329e3d6046055029d8fd5e71f13572bbd4863
Dagmara Korta <dagmara.korta@gmail.com>
2022-08-22 15:25:50 +0200

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_131311) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_131420) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_131311) do
     t.bigint "instrument_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "experience_level"
     t.index ["instrument_id"], name: "index_user_instruments_on_instrument_id"
     t.index ["user_id"], name: "index_user_instruments_on_user_id"
   end
