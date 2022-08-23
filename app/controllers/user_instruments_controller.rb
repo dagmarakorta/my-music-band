01b5e8d60c799af79d14afcb4b86ac39a5811c07
Dagmara Korta <dagmara.korta@gmail.com>
2022-08-23 18:13:26 +0200

    defined create for user_instruments

--- app/controllers/user_instruments_controller.rb
+++ app/controllers/user_instruments_controller.rb
@@ -1,2 +1,13 @@
 class UserInstrumentsController < ApplicationController
+  def create
+    @new_instrument = UserInstrument.new(user_instrument_params)
+    @new_instrument.user_id = current_user.id
+    @new_instrument.save
+    redirect_to user_path(current_user)
+  end
+
+  private
+  def user_instrument_params
+    params.require(:user_instrument).permit(:instrument_id)
+  end
 end
