42f13f0d70a0a69b9087b140a1bcd328fae3a46a
Dagmara Korta <dagmara.korta@gmail.com>
2022-08-28 09:46:16 +0200

    renamed instruments column instrument to name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_131420) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_28_074029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_131420) do
   end
 
   create_table "instruments", force: :cascade do |t|
-    t.string "instrument"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
