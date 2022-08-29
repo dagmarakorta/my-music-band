81c2b14039cf7337778aedee22b8713a78115c9f
Dagmara Korta <dagmara.korta@gmail.com>
2022-08-29 20:47:45 +0200

    defined update user_instrument and refactored

--- app/controllers/user_instruments_controller.rb
+++ app/controllers/user_instruments_controller.rb
@@ -1,4 +1,6 @@
 class UserInstrumentsController < ApplicationController
+  before_action :set_user_instrument, only: [:update, :destroy]
+
   def create
     @new_instrument = UserInstrument.new(user_instrument_params)
     @new_instrument.user_id = current_user.id
@@ -6,14 +8,23 @@ class UserInstrumentsController < ApplicationController
     redirect_to user_path(current_user)
   end
 
+  def update
+    @instrument.update(user_instrument_params)
+    redirect_to user_path(current_user)
+  end
+
   def destroy
-    @instrument = UserInstrument.find(params[:id])
     @instrument.destroy
     redirect_to user_path(current_user)
   end
 
   private
+
   def user_instrument_params
     params.require(:user_instrument).permit(:instrument_id, :experience_level)
+  end
+
+  def set_user_instrument
+    @instrument = UserInstrument.find(params[:id])
   end
 end
